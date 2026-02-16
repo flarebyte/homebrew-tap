5f4a76f19fee20f2debf635347b332e2bd3e9c71
Olivier Huin <olih@users.noreply.github.com>
2026-02-16 20:16:17 +0000

    Updated the Homebrew formula for rebec to v0.6.0 (#4)

--- Formula/baldrick-rebec.rb
+++ Formula/baldrick-rebec.rb
@@ -1,9 +1,9 @@
 class BaldrickRebec < Formula
   desc "Autonomous build automation tool and task runner"
   homepage "https://github.com/flarebyte/baldrick-rebec"
-  url "https://github.com/flarebyte/baldrick-rebec/releases/download/v0.5.0/baldrick-rebec-darwin-arm64"
-  sha256 "bf7b05c70c01e4408f5ae52bb3218533787e755dd0eb2a2cb1eaf3cfffdfd176"
-  version "0.5.0"
+  url "https://github.com/flarebyte/baldrick-rebec/releases/download/v0.6.0/baldrick-rebec-darwin-arm64"
+  sha256 "f8b4a5c8fed7e0cbd4e17e155bb1405a0b602c045dd85633262701058a2c0d00"
+  version "0.6.0"
 
   def install
     bin.install "baldrick-rebec-darwin-arm64" => "rbc"
