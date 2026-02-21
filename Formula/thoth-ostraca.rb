51f2245c6517c1b21e952f0759f8e1dc3be3766a
Olivier Huin <olih@users.noreply.github.com>
2026-02-21 19:47:12 +0000

    Patch Formula/thoth-ostraca.rb from 1.0.0 to 1.0.1

--- Formula/thoth-ostraca.rb
+++ Formula/thoth-ostraca.rb
@@ -1,9 +1,9 @@
 class ThothOstraca < Formula
   desc "A repository of interconnected data fragments archived for eternity by the scribe of the gods"
   homepage "https://github.com/flarebyte/thoth-ostraca"
-  url "https://github.com/flarebyte/thoth-ostraca/releases/download/v1.0.0/thoth-ostraca-darwin-arm64"
-  sha256 "313db68e6299b1963b479dc2f744531c23eedf0df4a54a55ba20d686fcc90097"
-  version "1.0.0"
+  url "https://github.com/flarebyte/thoth-ostraca/releases/download/v1.0.1/thoth-ostraca-darwin-arm64"
+  sha256 "baa0cb855958e37d1b8e5564ce42b0e2da98c3b3923f76ab382d154439c1232f"
+  version "1.0.1"
 
   def install
     bin.install "thoth-ostraca-darwin-arm64" => "thoth"
