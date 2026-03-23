da42ec89e3d9ea86c64dcb2203d3e32e7c35c7ca
Olivier Huin <olih@users.noreply.github.com>
2026-03-23 20:32:51 +0000

    Updated Formula/thoth-ostraca.rb to v1.2.0 with the new macOS arm64 asset URL and SHA256

--- Formula/thoth-ostraca.rb
+++ Formula/thoth-ostraca.rb
@@ -1,9 +1,9 @@
 class ThothOstraca < Formula
   desc "A repository of interconnected data fragments archived for eternity by the scribe of the gods"
   homepage "https://github.com/flarebyte/thoth-ostraca"
-  url "https://github.com/flarebyte/thoth-ostraca/releases/download/v1.0.1/thoth-ostraca-darwin-arm64"
-  sha256 "baa0cb855958e37d1b8e5564ce42b0e2da98c3b3923f76ab382d154439c1232f"
-  version "1.0.1"
+  url "https://github.com/flarebyte/thoth-ostraca/releases/download/v1.2.0/thoth-ostraca-darwin-arm64"
+  sha256 "9e32c4f35c8fa032798919eddacdd2d22391a4faad77ebb97cf1cb3427600f45"
+  version "1.2.0"
 
   def install
     bin.install "thoth-ostraca-darwin-arm64" => "thoth"
