f14416e568c47e77aa4a2c1df80e51270cec0b2b
Olivier Huin <olih@users.noreply.github.com>
2026-04-12 15:46:18 +0100

    Updated Formula/baldrick-seer.rb to v1.2.0.

--- Formula/baldrick-seer.rb
+++ Formula/baldrick-seer.rb
@@ -1,9 +1,9 @@
 class BaldrickSeer < Formula
   desc "Defining the horizon of possibility"
   homepage "https://github.com/flarebyte/baldrick-seer"
-  url "https://github.com/flarebyte/baldrick-seer/releases/download/v1.1.0/seer-darwin-arm64"
-  sha256 "c130a94b024b44aac3605f5d1b49f2c12ba2924acce1a14e7a215e6cfe92f963"
-  version "1.1.0"
+  url "https://github.com/flarebyte/baldrick-seer/releases/download/v1.2.0/seer-darwin-arm64"
+  sha256 "14b9f6a19525820d4a132468e04244957b00dd0ece5c9ce77c900995af1bc156"
+  version "1.2.0"
 
   def install
     bin.install "seer-darwin-arm64" => "seer"
