7f9725d0f78f737e3250fa336220221483f8ce14
Olivier Huin <olih@users.noreply.github.com>
2026-05-16 07:20:51 +0100

    Updated baldrick-flying-buttress.rb to version: 0.11.0

--- Formula/baldrick-flying-buttress.rb
+++ Formula/baldrick-flying-buttress.rb
@@ -1,9 +1,9 @@
 class BaldrickFlyingButtress < Formula
   desc "Divert the weight of design away from the code with a flying buttress"
   homepage "https://github.com/flarebyte/baldrick-flying-buttress"
-  url "https://github.com/flarebyte/baldrick-flying-buttress/releases/download/v0.10.0/baldrick-flying-buttress-darwin-arm64"
-  sha256 "fe73c6d38eb80d06075b29ea6d55ea5a5d023cf200dcff10309598892759c267"
-  version "0.10.0"
+  url "https://github.com/flarebyte/baldrick-flying-buttress/releases/download/v0.11.0/baldrick-flying-buttress-darwin-arm64"
+  sha256 "736ff08b64918aaf94b5fe9a8024ffd67929dc2927b137ea0ebc8e54a63be6b5"
+  version "0.11.0"
 
   def install
     bin.install "baldrick-flying-buttress-darwin-arm64" => "flyb"
